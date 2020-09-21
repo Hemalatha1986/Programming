dcfaa50b6a5d328e91e43d0ec9ef62e68b24e984
Hemalatha1986 <70719666+Hemalatha1986@users.noreply.github.com>
2020-09-21 10:40:39 +0200

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstMVCproject
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+
+            ViewBag.Greeting =
+            hour < 12
+            ? "Good Morning. Time is" + DateTime.Now.ToShortTimeString()
+            : "Good Afternoon. Time is " + DateTime.Now.ToShortTimeString();
+
+            return View();
+            
+        }
+
+
+    }
+
+    
+}
