6d7752bce279f369f5b6c1fbe7af31778a668ad5
Hemalatha1986 <70719666+Hemalatha1986@users.noreply.github.com>
2020-09-25 11:46:35 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,49 +1,84 @@
-﻿using System;
-using System.Threading.Tasks; 
-namespace asyncprogramming
+﻿using System.Threading;
+using System;
+using DLexample;
+
+namespace DLexample
 {
-    //test
-    class Program
+  
+    public class Account
     {
-        static void Main(string[] args)
+        double _balance;
+        int _id;
+        public Account(int id, double balance)
         {
-            callMethod();
-            Console.ReadKey();
+            this._balance = balance;
+            this._id = id;
+        }
+        public int ID // this will return the account ID
+        { 
+       get 
+            {return _id;}
+
+        }
+        public void Withdraw(double amount) //method to withdraw money
+        {
+            _balance -= amount;
+        }
+        public void Deposit(double amount) //method to deposit money
+        {
+            _balance += amount;
         }
 
-        public static async void callMethod()
-        {
-            Task<int> task = Method1();
-            Method2();
-            int count = await task;
-            Method3(count);
-        }
+    }
 
-        public static async Task<int> Method1()
+public class AccountManager
+{
+    Account _fromAccount;
+    Account _toAccount;
+    double _amountToTransfer;
+    public AccountManager(Account fromAccount,Account toAccount,double amountToTransfer)
+    {
+        this._fromAccount = fromAccount;
+        this._toAccount = toAccount;
+        this._amountToTransfer = amountToTransfer;
+    }
+    public void Transfer()
+    {
+         Console.WriteLine(Thread.CurrentThread.Name + " trying to acquire to lock on" + _fromAccount.ID.ToString());
+        lock(_fromAccount)
         {
-            int count = 0;
-            await Task.Run(() =>
+                Console.WriteLine(Thread.CurrentThread.Name + "acquire to lock on" + _fromAccount.ID.ToString());
+                 Thread.Sleep(1000);
+                Console.WriteLine(Thread.CurrentThread.Name + "Back in action and trying to acquire lock on" + _toAccount.ID.ToString());
+                lock (_toAccount)
             {
-                for (int i = 0; i < 100; i++)
-                {
-                    Console.WriteLine(" Method 1");
-                    count += 1;
-                }
-            });
-            return count;
-        }
-
-        public static void Method2()
-        {
-            for (int i = 0; i < 25; i++)
-            {
-                Console.WriteLine(" Method 2");
+                    Console.WriteLine("DeadLock");
+                _fromAccount.Withdraw(_amountToTransfer);
+                _toAccount.Deposit(_amountToTransfer);
             }
         }
-
-        public static void Method3(int count)
-        {
-            Console.WriteLine("Total count is " + count);
-        }
     }
+
+
+public static void Main()
+    {
+        Console.WriteLine("This is main method");
+        Account accountA = new Account(104, 5000);
+        Account accountB = new Account(105, 3000);
+
+        AccountManager accountManagerA = new AccountManager(accountA, accountB, 1000);//here Fromacc is accountA,Toacc is accountB
+        Thread T1 = new Thread(accountManagerA.Transfer);
+        T1.Name = "T1";
+        AccountManager accountManagerB = new AccountManager(accountB, accountA, 2000);//here Fromacc is accountB,Toacc is accountA
+            Thread T2 = new Thread(accountManagerB.Transfer);
+        T2.Name = "T2";
+        T1.Start();
+        T2.Start();
+        T1.Join();
+        T2.Join();
+        Console.WriteLine("Main completed");
+
+    
+}
+}
 }
