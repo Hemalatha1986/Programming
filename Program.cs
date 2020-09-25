ae0b5399c2e103995248f2c374a7973696a9d844
Hemalatha1986 <70719666+Hemalatha1986@users.noreply.github.com>
2020-09-25 13:09:46 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+ 
+namespace Mutexx
+{
+    class Program
+    {
+        static Mutex _mutex = new Mutex();
+        static void Main(string[] args)
+        {
+        
+            for (int i = 0; i < 5; i++)
+            {
+                new Thread(write).Start();
+            }
+            Console.ReadKey();
+        }
+        public static void write()
+
+        {
+            Console.WriteLine($"Thread{Thread.CurrentThread.ManagedThreadId} WAITING...");
+            _mutex.WaitOne();
+            Console.WriteLine($"Thread{Thread.CurrentThread.ManagedThreadId} WRITING...");
+            Thread.Sleep(2000);//WRITING for 5 sec
+            Console.WriteLine($"Thread{Thread.CurrentThread.ManagedThreadId} WRITING is completed...");
+            _mutex.ReleaseMutex();//READY NOW
+        }
+        
+    }
+}
